ad32c17add5da893ab50c9b6787a17ae2b2957fc
Taarik <taarik.siers@superbalist.com>
2020-03-02 07:42:23 +0200

    Fixed erroneous commit

--- 2d-array.php
+++ 2d-array.php
@@ -1,6 +1,7 @@
 <?php
 
-// https://www.hackerrank.com/challenges/beautiful-binary-string/problem?h_r=internal-search
+// https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+// THIS IS WIP shell copied from beautiful-binary-string.php
 
 $debug = $argv[1] ?? false;
 
